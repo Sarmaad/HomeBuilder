675787a21b530f588e44ff5a67a0c6d174680265
Sarmaad <sarmaad@gmail.com>
2018-06-28 21:56:11 +1000

    Add project files.

---
+++ HomeBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
